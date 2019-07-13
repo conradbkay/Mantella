3a367f071d530adf2d9d11b008f5982e40c5aa18
conradkay <conradkay123@gmail.com>
2019-07-13 17:55:00 -0400

    get the hecking apollo working

--- apollo.config.js
+++ apollo.config.js
@@ -3,10 +3,13 @@ module.exports = {
     localSchemaFile: './server/src/graphql/schema.graphql'
   },
   client: {
-    localSchemaFile: './server/src/graphql/schema.graphql',
-    service: 'mantella',
+    includes: ['client/src/**/*.{ts,tsx,gql,graphql,js}'],
+    excludes: ['**/node_modules/**/*'],
     tagName: 'gql',
-    url: 'http://localhost:4000/graphql'
+    service: {
+      name: 'mantella',
+      localSchemaFile: './server/src/graphql/schema.graphql'
+    }
   },
   engine: {
     apiKey: 'service:mantella:Ylb-gXcuSHCbWhBHn_Oa_A'
