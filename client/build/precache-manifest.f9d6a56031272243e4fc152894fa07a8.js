4cc956ae9622e52f59c551f617ff9568dc05755e
Conrad Kay <conradkay123@gmail.com>
2019-08-07 16:20:33 -0400

    the one

---
+++ client/build/precache-manifest.f9d6a56031272243e4fc152894fa07a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fe16dbdeee9b285e8cf",
+    "url": "/static/js/main.170d6b1b.chunk.js"
+  },
+  {
+    "revision": "d4fa32f079faf2a6a6af",
+    "url": "/static/js/2.25b56bf6.chunk.js"
+  },
+  {
+    "revision": "7fe16dbdeee9b285e8cf",
+    "url": "/static/css/main.d7330679.chunk.css"
+  },
+  {
+    "revision": "58a813623d76cdf80c94417db2ff92a1",
+    "url": "/index.html"
+  }
+];
