c7ce3d94d457f8cc064b45aa6c00bf07fbce9dcf
conradkay <conradkay123@gmail.com>
2020-11-17 21:28:35 -0500

    yep

--- server/src/server.ts
+++ server/src/server.ts
@@ -62,7 +62,7 @@ gqlServer.applyMiddleware({
 
 app.use(express.static(path.join(__dirname, '/../../client/build')))
 
-/*
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../../client/public/index.html'))
 })
@@ -71,6 +71,6 @@ app.get('*', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../../client/build/index.html'))
 })
-*/
+
 
 export default app
